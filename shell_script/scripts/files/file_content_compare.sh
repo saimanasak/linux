7c863d6099a30ceb7dac1e529ec4999844ea3695
Sai Manasa <muppedanvesh@gmail.com>
2023-12-28 20:56:12 -0600

    Updating files

--- shell_script/scripts/files/file_content_compare.sh
+++ shell_script/scripts/files/file_content_compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-file_path1="/c/Users/manas/OneDrive/Desktop/Chowdary/Techie/git/linux/shell_script/scripts/file1.txt"
-file_path2="/c/Users/manas/OneDrive/Desktop/Chowdary/Techie/git/linux/shell_script/scripts/file2.txt"
+file_path1="file1.txt"
+file_path2="file2.txt"
 
 if [ -r "$file_path1" ] && [ -r "$file_path2" ]; then
 	echo -e "\nFiles are readable!"
@@ -9,7 +9,7 @@ if [ -r "$file_path1" ] && [ -r "$file_path2" ]; then
 		echo -e "\nNow, checking for the matched content..."
 	    	sleep 5
 	    	pattern=$(cat "$file_path1")
-	    	grep -F "$pattern" "$file_path2" > "/c/Users/manas/OneDrive/Desktop/Chowdary/Techie/git/linux/shell_script/scripts/output.txt"
+	    	grep -F "$pattern" "$file_path2" > "output.txt"
 	    	echo -e "\nFinally, matched content has been copied!"
     	else
 		echo -e "\nFiles are empty!"
